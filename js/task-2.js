5eb53d8dfd023794584e58634cb0c3c4950bdb90
Yuriy Borys <iam618384@gmail.com>
2024-11-17 15:25:56 +0200

    Added index check for task 2

--- js/task-2.js
+++ js/task-2.js
@@ -14,7 +14,10 @@ class Storage {
     }
 
     removeItem(itemToRemove) {
-        this.#items.splice(this.#items.indexOf(itemToRemove), 1);
+        const indexToRemove = this.#items.indexOf(itemToRemove);
+        if (indexToRemove !== -1) {
+            this.#items.splice(indexToRemove, 1);
+        }
     }
 }
 
